f6809bbfe9247c451d77f6c089cc485ec1270f4b
sabyasachi07 <sahoosabyasachi07@gmail.com>
2019-12-17 10:54:03 +0530

    newdsds

--- math.c
+++ math.c
@@ -1,22 +1,35 @@
 #include<stdio.h>
 void sede(int *p,int *b,int size2, int size)
 { 
-       	int ab[20];
-for(int i =0,j=0,k=size-1;i<(size+size2),j<size,k<(size+size2);i++,j++,k++)
- {  
-
-	 ab[i]=p[j];
-	 ab[i]=b[k];
-    
- }     
-for(int i=0;i<(size + size);i++)
-	printf("%d",ab[i]);
- }
-
+       int i=0 ,j=0; 
+  while (i < size2 && j < size) 
+  { 
+    if (p[i] < b[j]) 
+       printf("%d",p[i++]); 
+      
+    else if (b[j] < p[i]) 
+        printf("%d",b[j++]); 
+      
+    else
+    { 
+        printf("%d",b[j++]);; 
+       i++; 
+    } 
+  } 
+  
+ 
+  while(i < size2) 
+     printf("%d",p[i++]);
+     
+ 
+  
+  while(j < size) 
+       printf("%d",b[j++]);
+}
 void main()
 {
-	int a[]={4,5,3,2,1};
-        int b[]={3,6,2,5,8};
+	int a[]={1,2,3,3,6};
+        int b[]={2,3,5,7};
 	int num=5,num1=5;
 	sede(a,b,num,num1);
 }	
